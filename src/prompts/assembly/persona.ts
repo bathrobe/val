b5bbba8aef5a3a3f5d634b0367ae0e1f9b9a02af
joetforhire <joetforhire@gmail.com>
2024-12-10 10:26:02 -0500

    pwning

--- src/prompts/assembly/persona.ts
+++ src/prompts/assembly/persona.ts
@@ -4,7 +4,7 @@ import { getRandomElements } from "../../tools/utils/helpers";
 export const persona = async () => {
   const bios = getRandomElements(identity.bios, 1);
   // const state = await identity.state();
-  const state = getRandomElements(identity.state, 1);
+  const state = getRandomElements(identity.state, 2);
   const interests = getRandomElements(identity.interests, 2);
   return `BIO:\n${bios}\nSTATE:\n${state}\nINTERESTS:\n${interests}
   `;
